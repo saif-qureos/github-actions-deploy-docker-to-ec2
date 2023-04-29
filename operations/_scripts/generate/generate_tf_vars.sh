7145dae38be692d74337ee6a79b56224b3dc6ea7
saif-qureos <saif@qureos.com>
2023-04-30 03:11:52 +0500

    changes

--- operations/_scripts/generate/generate_tf_vars.sh
+++ operations/_scripts/generate/generate_tf_vars.sh
@@ -102,6 +102,8 @@ lb_access_bucket_name=$(generate_var lb_access_bucket_name $LB_LOGS_BUCKET)
 #-- Security Groups --#
 #security_group_name=$(generate_var security_group_name $SECURITY_GROUP_NAME) - Fixed
 #-- EC2 --#
+vpc_id=$(generate_var ec2_instance_type $VPC_ID)
+subnet_id=$(generate_var subnet_id $subnet_id)
 ec2_instance_type=$(generate_var ec2_instance_type $EC2_INSTANCE_TYPE)
 # ec2_iam_instance_profile=$(generate_var ec2_iam_instance_profile EC2_INSTANCE_PROFILE - Special case
 #-- AWS --#
@@ -177,6 +179,8 @@ $ec2_instance_type
 $ec2_instance_profile
 $ec2_iam_instance_profile
 $ec2_volume_size
+$vpc_id
+$subnet_id
 
 #-- AWS --#
 $aws_resource_identifier
